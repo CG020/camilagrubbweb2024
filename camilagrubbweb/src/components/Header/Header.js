6413de39bda9310b44c53a6d3282b4cb79160ef2
CG020 <gmills020@gmail.com>
2024-05-13 12:30:07 -0700

    header image

---
+++ camilagrubbweb/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+import './Header.css';
+
+function Header() {   
+    return (
+        <div className="Header">
+          <h1><i><b>Camila Grubb</b></i></h1>
+        </div>
+      );
+}  
+
+export default Header;
