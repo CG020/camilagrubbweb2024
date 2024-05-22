d57674611f49ee92fafae1b45f9ad9f59e2a19be
CG020 <gmills020@gmail.com>
2024-05-22 12:09:01 -0700

    bio section added

--- camilagrubbweb/src/App.js
+++ camilagrubbweb/src/App.js
@@ -1,13 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Header from './components/Header/Header';
+import Bio from './components/Bio/Bio';
 
 function App() {
+  const [scrollY, setScrollY] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const headerOpacity = Math.max(1 - scrollY / 500, 0);
+
   return (
     <div className="App">
+
+      <div className='header-section' style={{ opacity: headerOpacity }}>
       <Header></Header>
+      </div>
+
+      <div className='bio-section'>
+        <Bio></Bio>
+      </div>
+
     </div>
   );
 }
---
+++ camilagrubbweb/src/components/Bio/Bio.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+import './Bio.css';
+
+function Bio() {
+    return(
+        <div className='Bio'>
+            <img src="./assets/originalImage.jpg" className="bio-image" alt="profile picture" />
+        </div>
+    );
+}
+
+export default Bio;
