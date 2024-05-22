e15bc37a35e61d48923606dcf32a70dacd49dacc
CG020 <gmills020@gmail.com>
2024-05-22 11:32:01 -0700

    navbar addition

--- camilagrubbweb/src/components/Header/Header.js
+++ camilagrubbweb/src/components/Header/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';  
 import './Header.css';
 import DropMenu from '../DropMenu/DropMenu';
+import Navbar from '../Navbar/Navbar';
 
 function Header() {   
     return (
+        <div>
+        <Navbar message='welcome' tabs={['bio','projects','involvement','gallery']}/>
         <div className="Header">
           <h1><i><b>Camila Grubb</b></i></h1>
           <DropMenu name='contact me' items={{email:'camilagrubb@gmail.com',
             phone:'5203609985', LinkedIn: 'www.linkedin.com/in/camila-grubb-8ab00b240', 
             GitHub: 'https://github.com/CG020'}}></DropMenu>
         </div>
+      </div>
       );
 }
 
