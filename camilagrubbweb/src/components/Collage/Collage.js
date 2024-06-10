638bb496dfa04cc7b110ce13bb305d6ecd4c72f3
CG020 <gmills020@gmail.com>
2024-06-10 11:52:04 -0700

    fixed the spanning issue in the collage setup

--- camilagrubbweb/src/components/Collage/Collage.js
+++ camilagrubbweb/src/components/Collage/Collage.js
@@ -8,9 +8,9 @@ function Collage() {
             <div className="collage-container">
                 <div className="panel edu">
                     <span className="hidden-text">
-                        I'm currently in my Senior Year pursuing my Bcahelor's of Science at the
+                        I'm currently in my Senior Year pursuing my Bachelor's of Science at the
                         University of Arizona. I am minoring in Business Administration at the Eller
-                        College of Business and earned by certificate in general Cybersecurity at UA South.
+                        College of Business and earned my certificate in general Cybersecurity at UA South.
                         
                     </span>
                     education
@@ -26,15 +26,20 @@ function Collage() {
                     internships</div>
                 <div className="panel involve">
                 <span className="hidden-text">
-                        Angel Heart is a Southern Arizona Based volunteer group that helps children have access to a warm and welcoming 
-                        community. Children get access to clothes, books, toys, and dependable volunteers there to help them grow and thrive away
-                        from the difficult living situations they come from. I have been lucky enough to volunteer for Pajama Project events including
-                        those that take place around the holidays where I get a chance to meet many kindhearted kids looking for a safe place to learn.
+                        Angel Heart is a Southern Arizona Based volunteer group that helps children have access to clothes, books, toys, and dependable volunteers there to help them grow and thrive.
+                        I have been lucky enough to volunteer for Pajama Project events including
+                        those that take place around the holidays where I get a chance to meet kids looking for a safe place to learn.
                         I've met lots of good people and it is a great honor to work among my fellow Tucsonans to better the community.
-                        
+
                     </span>
                     involvement</div>
-                <div className="panel docs">documents</div>
+                <div className="panel docs">
+                <span className="hidden-text">
+                    <embed src="file:///C:/Users/Gmill/Downloads/CamilaGrubbCV2024Updated%20(1).pdf" width="300" height="375" 
+                    type="CV/pdf"></embed>
+                    </span>
+                    
+                    documents</div>
             </div>
         </div>
     );
