ff59c66e53533c054ec1c58f7c204ade4f25dbda
Camila Grubb <gmills020@gmail.com>
2024-09-03 16:38:59 -0700

    updates

--- camilagrubbweb/src/components/Collage/Collage.js
+++ camilagrubbweb/src/components/Collage/Collage.js
@@ -8,35 +8,32 @@ function Collage() {
             <div className="collage-container">
                 <div className="panel edu">
                     <span className="hidden-text">
-                        I'm currently in my Senior Year pursuing my Bachelor's of Science at the
-                        University of Arizona. I am minoring in Business Administration at the Eller
-                        College of Business and earned my certificate in general Cybersecurity at UA South.
+                        - Bachelor's of Science Senior at the University of Arizona. <br></br>
+                        - Minoring in Business Administration at the Eller College of Business. <br></br>
+                        - Earned certificate in General Cybersecurity at UA South.
                         
                     </span>
                     education
                 </div>
                 <div className="panel internship">
                     <span className="hidden-text">
-                        I have experience in working on collaborative research and data analysis from ongoing work at the Banner Medical
-                        Center under the University of Arizona. I am involved in the R25 ADVANCE renal research internship at UA where 
-                        I work with several UA students on various experiments involving research on technology and its uses in the medical field.
-                        My previous work in internships that are now completed are recorded in my documents.
-
+                        - Collaborative research and data analysis from ongoing work at the Banner Medical
+                        Center under the University of Arizona. <br></br>
+                        - R25 ADVANCE Renal Research Summer internships at UA. Studying applications of medical technology.<br></br> 
+                        - UA Design Labs Internship. Partnered with APEX Applied Technologies, designed a software based solution to 
+                        involvement issue.
                     </span>
                     internships</div>
                 <div className="panel involve">
                 <span className="hidden-text">
                         Angel Heart is a Southern Arizona Based volunteer group that helps children have access to clothes, books, toys, and dependable volunteers.
-                        I have been lucky enough to volunteer for Pajama Project events including
-                        those that take place around the holidays where I get a chance to meet kids looking for a safe place to learn.
-                        I've met lots of good people and it is a great honor to work among my fellow Tucsonans.
-
+                        I volunteer for Pajama Project events including holidays special events where I meet kids looking for a safe place to learn.
                     </span>
                     involvement</div>
                 <div className="panel docs">
                 <span className="hidden-text">
                     <div>
-                        <object width="500" height="400" data={require('./assets/CamilaGrubbCV2024Updated (2) (1).pdf')} type="application/pdf">   </object>
+                        <object width="500" height="400" data={require('./assets/CamilaGrubbCV2024.pdf')} type="application/pdf">   </object>
                     </div>
                     </span>
                     
